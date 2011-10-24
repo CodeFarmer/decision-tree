6172c543674a98e6cf7fbf456d6cdc4de54bb86c
CodeFarmer <joel.gluth@gmail.com>
2011-10-24 12:14:29 +0100

    Added lein skeleton

---
+++ src/decision_tree/core.clj
@@ -0,0 +1,1 @@
+(ns decision-tree.core)
---
+++ test/decision_tree/test/core.clj
@@ -0,0 +1,6 @@
+(ns decision-tree.test.core
+  (:use [decision-tree.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
