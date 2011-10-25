8fbca4fe9dc0cfe800cd7400ad05d8e5c00f2015
CodeFarmer <joel.gluth@gmail.com>
2011-10-25 16:27:21 +0100

    Added general seq-to-map-of-outputs-to-input-subeqs function

--- src/decision_tree/core.clj
+++ src/decision_tree/core.clj
@@ -7,5 +7,27 @@
 
   "Given a list of pairs if (input, output), calculate the entropy of the list with respect to output"
 
-  (let [n (count alist) counts (vals (frequencies (map #(nth % 1) alist)))]
+  (let [n (count alist)
+        counts (vals (frequencies (map #(nth % 1) alist)))]
+    
     (apply + (map #(* (- %) (log2 %)) (map #(/ % n) counts)))))
+
+
+(defn map-by
+  
+  ([afn aseq]
+     
+     "Given function and a seq, return a map of values the function can take when using members of the seq as an argument, to sub-seqs containing the parts of seq corresponding to those outputs"
+
+     (map-by {} afn aseq))
+
+  ([acc afn aseq]
+
+     (if (empty? aseq)
+       acc
+       (let [i (first aseq) o (afn i) inputs (get acc o)]
+         (if inputs
+           (recur
+            (assoc acc o (conj inputs i)) afn (rest aseq))
+           (recur
+            (assoc acc o [i]) afn (rest aseq)))))))
--- test/decision_tree/test/core.clj
+++ test/decision_tree/test/core.clj
@@ -12,25 +12,38 @@
   (is (= 1.0 (log2 2)))
   (is (= 2.0 (log2 4))))
 
+(deftest test-map-by
+
+  (is (= {} (map-by identity [])) "should return an empty map when the input seq is empty")
+
+  (is (= {:a [:a], :b [:b], :c [:c]} (map-by identity [:a :b :c])) "should return a map of x to a list containing each of x, for each member of a seq with no repeats when passed the identity function")
+
+  (is (= {:a [:a :a], :b [:b]} (map-by identity [:a :a :b])) "should return a map of x followed by the repeated elements, for a seq with repeats and the identity function")
+
+  (is (= {1 [{:a 1 :b 2}], 2 [{:a 2 :b 1} {:a 2 :b 2}]} (map-by :a [{:a 1 :b 2} {:a 2 :b 1} {:a 2 :b 2}])) "Should be able to partition a list of maps, using a keyword as the function argument (since this is the actual use case for decision-tree)"))
+
+
 ;; stealing the rspec tests from decision_tree.rb
 
 (deftest test-entropy
 
   (is (= 0 (entropy [])) "entropy should be zero for an empty data set")
   
-  (is (within? 0.0005 0.940 (entropy [ 
-                                     [1,  "Y"]
-                                     [2,  "Y"]
-                                     [3,  "Y"]
-                                     [4,  "Y"]
-                                     [5,  "Y"]
-                                     [6,  "Y"]
-                                     [7,  "Y"]
-                                     [8,  "Y"]
-                                     [9,  "Y"]
-                                     [10, "N"]
-                                     [11, "N"]
-                                     [12, "N"]
-                                     [13, "N"]
-                                     [14, "N"]]))
+  (is (within? 0.0005 0.940
+               (entropy [ 
+                         [1,  "Y"]
+                         [2,  "Y"]
+                         [3,  "Y"]
+                         [4,  "Y"]
+                         [5,  "Y"]
+                         [6,  "Y"]
+                         [7,  "Y"]
+                         [8,  "Y"]
+                         [9,  "Y"]
+                         [10, "N"]
+                         [11, "N"]
+                         [12, "N"]
+                         [13, "N"]
+                         [14, "N"]]))
       "should match the value from the Mitchell book"))
+
