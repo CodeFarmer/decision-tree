94197bf66ec26efe827dac92fb5b81e7a42f2d2a
CodeFarmer <joel.gluth@gmail.com>
2011-11-10 18:59:30 +0000

    Life without swank is hard

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject decision-tree "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]])
+  :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]
+                     [swank-clojure "1.3.2"]])
